42b51fae10e367a2d388910864ee90e8785de642
Ihor <sadovenko555@gmail.com>
2025-02-01 17:44:18 +0200

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/gorilla_armor/', // Заменить на реальное имя репозитория
+  build: {
+    outDir: 'docs',
+  },
 });
