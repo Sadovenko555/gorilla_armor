534b03ca322cdfa2fd01de7140f13152eb090c5e
Ihor <sadovenko555@gmail.com>
2025-02-01 21:55:43 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
- //base: '/gorilla_armor/', // Убираем для локальной разработки
+  base: '/gorilla_armor/', // Убираем для локальной разработки
 });
